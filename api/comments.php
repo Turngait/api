27cc501d4b59280d9bd3628877bd93fc5c2c2794
Turngait <wulzin.t@gmail.com>
2021-11-26 13:10:56 +0400

    Add processing of method

--- api/comments.php
+++ api/comments.php
@@ -50,20 +50,34 @@ function useRouting(CommentsController $commentController, Responce $responce, s
     $commentController->sendAllComments();
   }
   elseif ($method === 'POST') {
-    try {
-      $rawData = file_get_contents('php://input');
-      $data = json_decode($rawData);
-      if (!$data) {
-        $responce->sendExceptionError('Wrong format', 400);
-        die();
-      }
-      $commentController->saveNewComment($data);
-    }
-    catch (Exception $ex) {
-      error_log('Exception error: '.$ex->getMessage(), 0);
-      $responce->sendExceptionError($ex->getMessage());
+    $commentController->saveNewComment(getDataFromRequest($responce));
+  }
+  else {
+    $responce->sendExceptionError('Method Not Allowed', 405);
+  }
+}
+
+
+/**
+ * Получает данные из запроса, проверяет их наличие и возвращает
+ * @param Responce $responce
+ * @return array
+ */
+function getDataFromRequest(Responce $responce): ?array
+{
+  try {
+    $rawData = file_get_contents('php://input');
+    $data = json_decode($rawData);
+    if (!$data) {
+      $responce->sendExceptionError('Wrong format', 400);
       die();
     }
+    return $data;
+  }
+  catch (Exception $ex) {
+    error_log('Exception error: '.$ex->getMessage(), 0);
+    $responce->sendExceptionError($ex->getMessage());
+    die();
   }
 }
 
