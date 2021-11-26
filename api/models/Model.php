5f54ba57aa40c1208119206368577392c7899f68
Turngait <wulzin.t@gmail.com>
2021-11-26 12:40:09 +0400

    Add class Model

--- api/models/Model.php
+++ api/models/Model.php
@@ -1,6 +1,47 @@
 <?php
 
+/**
+ * Модель для работы с БД
+ */
 class Model
 {
+  protected PDO $writeDB;
+  protected PDO $readDB;
+  protected PDOStatement $query;
 
+  protected function __construct(PDO $writeDB, PDO $readDB)
+  {
+    $this->writeDB = $writeDB;
+    $this->readDB = $readDB;
+  }
+
+  protected function prepareReadDB(string $insertStatement)
+  {
+    $this->query = $this->readDB->prepare($insertStatement);
+  }
+
+  protected function prepareInsertToDB(string $insertStatement): void
+  {
+    $this->query = $this->_writeDB->prepare($insertStatement);
+  }
+
+  protected function bindParam(string $param, string $value, int $type = PDO::PARAM_STR): void
+  {
+    $this->query->bindParam($param, $value, $type);
+  }
+
+  protected function save(): void
+  {
+    $this->query->execute();
+  }
+
+  protected function getLastInsertedID(): ?string
+  {
+    return $this->_writeDB->lastInsertId();
+  }
+
+  protected function getRowCount(): int
+  {
+    return $this->query->rowCount();
+  }
 }
